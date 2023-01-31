4d0d3a168c143a25740141c5ee58fadfa9c53406
Bilel Hofer <bilel.hofer@ik.me>
2023-01-31 15:44:25 +0100

    Ajout de l'edit texte et du bouton envoyer

--- app/src/main/java/com/example/qrscanlocalisation/MapFragment.java
+++ app/src/main/java/com/example/qrscanlocalisation/MapFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,18 +19,48 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.text.DecimalFormat;
+
 public class MapFragment extends Fragment {
 
     private LatLng lat;
+    private LatLng oldLat;
+    private PageViewModel pageViewModel;
+
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable;
+
     // constructor
     public MapFragment(LatLng _lat) {
         // Required empty public constructor
         lat = _lat;
+        oldLat = new LatLng(0, 0);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // initialise ViewModel
+        pageViewModel = ViewModelProviders.of(requireActivity()).get(PageViewModel.class);
+
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                if (oldLat != lat) {
+                    pageViewModel.setCoordinateLat("Ma localisation: " + String.valueOf(df.format(lat.latitude)) + "," + String.valueOf(df.format(lat.longitude)));
+                    oldLat = lat;
+                }
+
+                // Permet au handler de ce répéter
+                mHandler.postDelayed(mRunnable, 100);
+            }
+        };
+
+        // Lance le handler
+        mHandler.postDelayed(mRunnable, 100);
     }
 
     @Override
@@ -63,6 +96,8 @@ public class MapFragment extends Fragment {
                         MarkerOptions markerOptions = new MarkerOptions();
                         // Set position of marker
                         markerOptions.position(latLng);
+                        // Upadte this.lat
+                        lat = latLng;
                         // Set title of marker
                         markerOptions.title(latLng.latitude + " : " + latLng.longitude);
                         // Remove all marker
